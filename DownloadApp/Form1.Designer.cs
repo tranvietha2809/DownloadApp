53ee5b2b35dc14385f5b463aa3bbe7622ced9b1b
Ha Tran <tranvietha.2809@gmail.com>
2019-01-21 22:42:38 +0200

    Initial commit

--- DownloadApp/Form1.Designer.cs
+++ DownloadApp/Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.URLBox = new System.Windows.Forms.TextBox();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // URLBox
+            // 
+            this.URLBox.Location = new System.Drawing.Point(12, 12);
+            this.URLBox.Name = "URLBox";
+            this.URLBox.Size = new System.Drawing.Size(411, 20);
+            this.URLBox.TabIndex = 0;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Location = new System.Drawing.Point(146, 88);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(141, 55);
+            this.DownloadButton.TabIndex = 1;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 38);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(381, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(435, 262);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.URLBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox URLBox;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- DownloadApp/Form1.cs
+++ DownloadApp/Form1.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace DownloadApp
 {
     public partial class Form1 : Form
     {
+        WebClient wc = new WebClient();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            string URLToDownload = URLBox.Text;
+            string pattern = @"\.\w{2,4}($|\?)";
+            Match FileExtension = Regex.Match(URLToDownload, pattern);
+            string Filename = DateTime.Now.ToString("dd") + DateTime.Now.ToString("MM") + DateTime.Now.ToString("yy") + DateTime.Now.ToString("hh") + DateTime.Now.ToString("mm") + DateTime.Now.ToString("ss") + FileExtension;
+            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+            wc.DownloadFileAsync(
+                new Uri(URLBox.Text),
+                Filename);
+        }
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
